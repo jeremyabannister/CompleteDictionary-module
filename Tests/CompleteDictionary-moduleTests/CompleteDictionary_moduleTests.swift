c35084021a1e65b22414b6118f6d32831dceb0ef
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-03-27 22:05:56 +0200

    Initial Commit

---
+++ Tests/CompleteDictionary-moduleTests/CompleteDictionary_moduleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CompleteDictionary_module
+
+final class CompleteDictionary_moduleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CompleteDictionary_module().text, "Hello, World!")
+    }
+}
